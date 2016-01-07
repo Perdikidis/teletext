875315ab1b71004677e23328c635a76434357aa1
Perdikidis <vp15272@my.bristol.ac.uk>
2016-01-07 23:02:55 +0000

    07-01-2016/23:02

--- teletext.c
+++ teletext.c
@@ -2,12 +2,10 @@
 
 int main(void)
 {
-
    Cell board[HEIGHT][WIDTH];
-   int a=20, test=0;
 
    read_file("lfc.m7.txt", board);
-   set(board);
+   set_board(board);
    printf("\n\n\nTwra exoume to board\n\n\n\n" );
    print_board(board);
    return 0;
@@ -16,7 +14,6 @@ int main(void)
 
 void read_file(char *fileName, Cell board[HEIGHT][WIDTH]){
 
-   char c;
    int i, j;
    size_t items;
    FILE *fp;
@@ -45,17 +42,28 @@ void read_file(char *fileName, Cell board[HEIGHT][WIDTH]){
    fclose(fp);
 }
 
-void set( Cell board[HEIGHT][WIDTH]){
+void set_board( Cell board[HEIGHT][WIDTH]){
 
-   int i, j, prevFore, prevBack;
+   int i, j, prevFore, prevBack, prevMode;
 
    for (i=0; i<HEIGHT; i++){
       for (j=0; j<WIDTH; j++){
-         if (j == 0){
+         if (j == 0){ /* set default settings in new line */
             board[i][j] = reset_settings(board[i][j]);
             prevFore = board[i][j].foreColour;
             prevBack = board[i][j].backColour;
+            prevMode = board[i][j].Mode;
          }
+         /* the range of alphanumeric codes */
+         if (( board[i][j].character >= 0x81) && (board[i][j].character <= 0x87)){
+            board[i][j].Mode = Alphanumeric;
+            prevMode = Alphanumeric;
+         }/* the range of graphic codes */
+         if (( board[i][j].character >= 0x91) && (board[i][j].character <= 0x97)){
+            board[i][j].Mode =  Graphics;
+            prevMode = Graphics;
+         }
+         board[i][j].Mode = prevMode;
          board[i][j] = set_foreground(board[i][j], &prevFore);
          board[i][j] = set_background(board[i][j], &prevBack, prevFore);
          board[i][j] = set_character(board[i][j]);
@@ -63,21 +71,26 @@ void set( Cell board[HEIGHT][WIDTH]){
    }
 }
 
- /* isws xreiastei na kanw to c pointer 'h na to valw na epistrefei cell */
 Cell reset_settings( Cell c){
 
    c.foreColour = WHITE;
    c.backColour = BLACK;
    c.Height = Single;
+   c.Mode = Alphanumeric;
+   c.Type = Contiguous;
    return c;
 }
 
 Cell set_foreground(Cell c, int *prevFore){
 
-   if (( c.character >= 0x81) && ( c.character <= 0x87)){
+   /* get new colour if the code is right*/
+   if (( c.character >= RED) && ( c.character <= WHITE)){
       c.foreColour = (int) c.character;
    }
-   else{
+   else if ((c.character >= 0x91) && (c.character <= 0x97)){
+      c.foreColour = (int) c.character - 16;
+   }
+   else{ /* else retain the previous one */
       c.foreColour = *prevFore;
    }
    /* set the previus foreground colour for our next cell */
@@ -89,7 +102,7 @@ Cell set_background(Cell c , int *prevBack, int prevFore){
 
    if ( c.character == BLACKBACKGROUND){
       c.backColour = BLACK;
-   }
+   } /*new background will get the colour of the previous foreground colour*/
    else if ( c.character == NEWBACKGROUND){
       c.backColour = prevFore;
    }
@@ -107,7 +120,37 @@ Cell set_character(Cell c){
    if ( c.character < SPACE){
       c.character = SPACE;
    }
+   if (c.Mode == Graphics){
+      /* Everything, but the blast through text*/
+      if ((c.character < 0xc0) || (c.character >=0xe0) ){
+         c = encode_graphics(c);
+         c.character = SPACE;
+      }
+   }
    return c;
+}
+
+Cell encode_graphics(Cell c){
+
+   int code = c.character - BASECODE;
+
+
+   c.pixel.r_bot = code / 64;/* check if the bottom right sixel is active */
+   code = code % 64;/* code gets the remainder of the division */
+   /* doing the same till the top left sixel */
+   c.pixel.l_bot = code / 16;
+   code = code % 16;
+   c.pixel.r_mid = code / 8;
+   code = code % 8;
+   c.pixel.l_mid = code / 4;
+   code = code % 4;
+   c.pixel.r_top = code / 2;
+   code = code % 2;
+   c.pixel.l_top = code / 1;
+   /* I know that these are magic numbers from the sixels
+   But when I #defined them, they were uglier and less readable */
+   return c;
+
 }
 
 void print_board(Cell board[HEIGHT][WIDTH]){
@@ -116,21 +159,59 @@ void print_board(Cell board[HEIGHT][WIDTH]){
    SDL_Simplewin sw;
    fntrow fontdata[FNTCHARS][FNTHEIGHT];
 
+
    SDL_Init(SDL_INIT_EVERYTHING);
    Neill_SDL_Init(&sw);
    Neill_SDL_ReadFont(fontdata, FNTFILENAME);
+   /*edw kanoume draw to background kai*/
    for (i=0; i<HEIGHT; i++){
       printf("\n");
       for (j=0; j<WIDTH; j++){
-         Neill_SDL_DrawCell(&sw, fontdata, board[i][j]  , j*FNTWIDTH, 20*i);
+         Neill_SDL_DrawCell(&sw, fontdata, board[i][j]  , j*FNTWIDTH, FNTHEIGHT*i);
+         Vlasis_draw_rect(&sw, board[i][j], i*FNTHEIGHT, j*FNTWIDTH);
          SDL_RenderPresent(sw.renderer);
          printf("%c",/*(int) board[i][j]*/board[i][j].character - 128 );
       }
    }
    SDL_UpdateWindowSurface(sw.win);
-   SDL_Delay(10000);
+   SDL_Delay(100000);
 
    SDL_Quit();
+}
+
+void Vlasis_draw_rect(SDL_Simplewin *sw, Cell c, int y, int x){
+
+   if( c.pixel.l_top == ACTIVE){
+      light_pixel(sw, y, x, c.foreColour);
+   }
+   if( c.pixel.l_mid == ACTIVE){
+      light_pixel(sw, y + PIXEL_H, x, c.foreColour);
+   }
+   if( c.pixel.l_bot == ACTIVE){
+      light_pixel(sw, y + (2*PIXEL_H), x, c.foreColour);
+   }
+   if( c.pixel.r_top == ACTIVE){
+      light_pixel(sw, y, x + PIXEL_W, c.foreColour);
+   }
+   if( c.pixel.r_mid == ACTIVE){
+      light_pixel(sw, y + PIXEL_H, x + PIXEL_W, c.foreColour);
+   }
+   if( c.pixel.r_bot == ACTIVE){
+      light_pixel(sw, y + (2*PIXEL_H), x + PIXEL_W, c.foreColour);
+   }
+
+}
+
+void light_pixel(SDL_Simplewin *sw, int y, int x, int colour){
+
+   SDL_Rect rect;
+
+   rect.x = x;
+   rect.y = y;
+   rect.w = PIXEL_W;
+   rect.h = PIXEL_H;
+   select_colour(sw, colour);
+   SDL_RenderFillRect( sw->renderer, &rect);
 }
 
 void Neill_SDL_Init(SDL_Simplewin *sw){
@@ -144,7 +225,7 @@ void Neill_SDL_Init(SDL_Simplewin *sw){
 
    sw->finished = 0;
 
-   sw->win= SDL_CreateWindow("SDL Window",
+   sw->win= SDL_CreateWindow("Vlasis Teletext",
                           SDL_WINDOWPOS_UNDEFINED,
                           SDL_WINDOWPOS_UNDEFINED,
                           WWIDTH, WHEIGHT,
@@ -198,10 +279,10 @@ void Neill_SDL_DrawCell(SDL_Simplewin *sw, fntrow fontdata[FNTCHARS][FNTHEIGHT],
 void select_colour( SDL_Simplewin *sw, int colour ){
 
    switch (colour) {
-      case RED:/*               R,   G, B */
+      case RED:/*                    R,   G, B */
          Neill_SDL_SetDrawColour(sw, 255, 0, 0);
          break;
-      case GREEN:
+      case GREEN: /* edw na valw green or greengraphics */
          Neill_SDL_SetDrawColour(sw, 0, 255, 0);
          break;
       case YELLOW:
